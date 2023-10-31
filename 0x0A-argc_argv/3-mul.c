9e48082c83f89cacca36ee1d705a087dca536ff5
otun ganiyu <otun.ganiyu@gmail.com>
2023-10-31 13:55:59 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,7 +9,7 @@
 *
 * Return: 0
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
 int x, y;
 
@@ -23,7 +23,7 @@ return (1);
 
 x = atoi(argv[1]);
 y = atoi(argv[2]);
-printf("%d\n", x * y);
+printf("%d\n", x *y);
 
 
 return (0);
